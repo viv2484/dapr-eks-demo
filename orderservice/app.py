71fbbd949c4468a83bc72d0ced47c1a9570857e0
viv2484 <82228918+viv2484@users.noreply.github.com>
2026-01-20 08:28:06 +0530

    Update app.py

--- orderservice/app.py
+++ orderservice/app.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
 
-# app = FastAPI()
-
-# @app.post("/orders")
-# def receive(order: dict):
-#     print("Received order:", order)
-#     return {"status": "processed"}
-
-from fastapi import FastAPI
-from dapr.ext.fastapi import DaprApp
-
 app = FastAPI()
-dapr_app = DaprApp(app)
 
-@dapr_app.subscribe(pubsub_name="order-pubsub", topic="orders")
-async def handle_order(data: dict):
-    print(f"Received order: {data}")
-    return {"status": "ok"}
+@app.post("/orders")
+def receive(order: dict):
+     print("Received order:", order)
+     return {"status": "processed"}
+
 
